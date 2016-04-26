61719a53bc618a40fc564de011e525b681bf5e1b
franchescamansour <cocomansour@Franchesca.local>
2016-04-26 17:05:45 -0400

    Added JS
    added js to the how many calories, show/hide

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var $bubbleburp = $('.bubbleburp');
 
+var $howmany = $('.howmany');
+var $fooditems = $('.fooditems');
+
 $bubbleburp.waypoint(function (){
   $bubbleburp.addClass('js-bubble-animate');
 }, { offset: '-5%' });
+
+$howmany.on('click', function () {
+  $fooditems.toggleClass('showbox');
+});
