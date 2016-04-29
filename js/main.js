8ffa7d8fe2834fce91649c9f578debd69cc4e17c
franchescamansour <cocomansour@Franchesca.local>
2016-04-29 19:51:10 -0400

    Markbot

--- js/main.js
+++ js/main.js
@@ -3,13 +3,10 @@ var $bubbleburp = $('.bubbleburp');
 var $howmany = $('.howmany');
 var $fooditems = $('.fooditems');
 
-$bubbleburp.waypoint(function (){
+$bubbleburp.waypoint(function () {
   $bubbleburp.addClass('js-bubble-animate');
-}, { offset: '-5%' });
+}, {offset: '-5%'});
 
 $howmany.on('click', function () {
   $fooditems.toggleClass('showbox');
 });
-
-
-
